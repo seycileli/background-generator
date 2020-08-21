0c0b13f09effb62ef8ef7a47a00ed8e8e18bbcbd
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-08-21 16:37:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const css = document.querySelector("h3");
+const colorOne = document.querySelector(".colorOne");
+const colorTwo = document.querySelector(".colorTwo");
+const body = document.getElementById("generator");
+const random = document.querySelector(".random");
+
+/* Generate colors */
+function setColor() {
+	body.style.background = 
+	"linear-gradient(to right, " + 
+	colorOne.value + 
+	", " + 
+	colorTwo.value + ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+colorOne.addEventListener("input", setColor);
+colorTwo.addEventListener("input", setColor);
+
+/* Random color generator */
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+ 
+function randomColor() {
+  colorOne.value = getRandomColor();
+  colorTwo.value = getRandomColor();
+  setColor();
+}
+
+random.addEventListener("click", randomColor);
+
+document.documentElement.style.setProperty('--animate-duration', '2s');
